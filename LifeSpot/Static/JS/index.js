ef7cb5d771191aa3273cd981d44f908930ac4a1c
sailor-moon-93 <xandir1993@gmail.com>
2024-05-13 20:01:52 +0300

    first js script

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,15 @@
+﻿// Запрос имени у пользователя
+var userName = prompt("Пожалуйста, введите ваше имя:");
+
+// Проверка на пустой ввод или нажатие "Отмена"
+if (userName === null || userName === "") {
+    alert("Вы не ввели имя. Вы будете перенаправлены");
+    window.location.href = "https://www.google.com";
+} else {
+    // Вывод имени и количества символов
+    var nameLength = userName.length;
+    alert("Привет, " + userName + "! В вашем имени " + nameLength + " символов.");
+}
+
+
+let elements = document.getElementsByTagName('*');
