ac50f884110587f5de20ae966a278b04f5d6d19f
sailor-moon-93 <xandir1993@gmail.com>
2024-05-15 22:22:38 +0300

    js testing

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,15 +1,8 @@
-﻿// Запрос имени у пользователя
-var userName = prompt("Пожалуйста, введите ваше имя:");
+﻿// Получаем все элементы на странице
+var allElements = document.getElementsByTagName("*");
 
-// Проверка на пустой ввод или нажатие "Отмена"
-if (userName === null || userName === "") {
-    alert("Вы не ввели имя. Вы будете перенаправлены");
-    window.location.href = "https://www.google.com";
-} else {
-    // Вывод имени и количества символов
-    var nameLength = userName.length;
-    alert("Привет, " + userName + "! В вашем имени " + nameLength + " символов.");
-}
+// Считаем количество элементов
+var elementCount = allElements.length;
 
-
-let elements = document.getElementsByTagName('*');
+// Выводим alert с количеством элементов
+alert("Total elements on the page: " + elementCount);
