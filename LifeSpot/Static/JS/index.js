2227fb4910ca9cc7e9fe9717310c53b3abca1b43
G <xandir1993@gmail.com>
2024-05-23 21:51:42 +0300

    search field changes

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -6,3 +6,9 @@ var elementCount = allElements.length;
 
 // Выводим alert с количеством элементов
 alert("Total elements on the page: " + elementCount);
+
+<input type="text" oninput="    // Получим все элементы input с нашей страницы    let elements = document.getElementsByTagName('input');
+ // Получим первый элемент DOM в коллекции
+ let element = elements[0]; // Получим строковое значение
+ let elementText = element.value;
+  // Выведем во всплывающее окно alert(elementText)" size=50 %>
