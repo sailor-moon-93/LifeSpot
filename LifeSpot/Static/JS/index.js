c5b58e96989055aee72ad649e34e043c24aebeb8
G <xandir1993@gmail.com>
2024-05-25 15:42:13 +0300

    починил поиск и фильтрацию видеоконтейнеров

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,14 +1,9 @@
-﻿// Получаем все элементы на странице
-var allElements = document.getElementsByTagName("*");
+﻿//// Получаем все элементы на странице
+//var allElements = document.getElementsByTagName("*");
 
-// Считаем количество элементов
-var elementCount = allElements.length;
+//// Считаем количество элементов
+//var elementCount = allElements.length;
 
-// Выводим alert с количеством элементов
-alert("Total elements on the page: " + elementCount);
+//// Выводим alert с количеством элементов
+//alert("Total elements on the page: " + elementCount);
 
-<input type="text" oninput="    // Получим все элементы input с нашей страницы    let elements = document.getElementsByTagName('input');
- // Получим первый элемент DOM в коллекции
- let element = elements[0]; // Получим строковое значение
- let elementText = element.value;
-  // Выведем во всплывающее окно alert(elementText)" size=50 %>
